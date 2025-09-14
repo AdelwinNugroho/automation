a24ffd6e02f4317f78f502c22e34f340e52e0124
Adelwin Nugroho Liandy <100504259+AdelwinNugroho@users.noreply.github.com>
2025-09-14 13:14:41 +0700

    Update devices.py

--- devices.py
+++ devices.py
@@ -4,15 +4,15 @@
 DEVICES = [
     {
         "device_type": "cisco_xr",
-        "host": "R3.STA.PE-MOBILE.1",
+        "host": "",
         "username": "930435",
         "password": "12345Qwer"
     },
     {
         "device_type": "cisco_xr",
-        "host": "R3.STA.PE-MOBILE.2",
-        "username": "930435",
-        "password": "12345Qwer"
+        "host": "",
+        "username": "",
+        "password": ""
     },
     # Tambahkan perangkat Cisco lainnya di sini
 ]
@@ -20,15 +20,15 @@ DEVICES = [
 # Konfigurasi untuk Jumphost SSI
 JUMPHOST_1 = {
     "device_type": "windows",
-    "host": "100.75.49.116",
-    "username": "user1",
-    "password": "sisindokom@sshd1"
+    "host": "",
+    "username": "",
+    "password": ""
 }
 
 # Konfigurasi untuk Jumphost Telkom
 JUMPHOST_2 = {
     "device_type": "terminal",
-    "host": "10.62.170.56",
-    "username": "930435",
-    "password": "Razor301412"
+    "host": "",
+    "username": "",
+    "password": ""
 }
