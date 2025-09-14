381e64e46ea38cf83683291a1448b762abe041d2
Adelwin Nugroho Liandy <100504259+AdelwinNugroho@users.noreply.github.com>
2025-09-14 13:15:50 +0700

    Update devices.py

--- devices.py
+++ devices.py
@@ -3,29 +3,29 @@
 
 JUMPHOST_1 = {
     "device_type": "windows",
-    "host": "100.75.49.116",
-    "username": "user1",
-    "password": "sisindokom@sshd1",
+    "host": "",
+    "username": "",
+    "password": "",
 }
 
 JUMPHOST_2 = {
     "device_type": "terminal",
-    "host": "10.62.170.56", 
-    "username": "930435",
-    "password": "Razor301412",
+    "host": "", 
+    "username": "",
+    "password": "",
 }
 
 DEVICES = [
     {
         "device_type": "cisco_xr",
-        "host": "R3.STA.PE-MOBILE.1",  # Ganti dengan IP atau hostname perangkat
-        "username": "930435",
-        "password": "12345Qwer",
+        "host": "",  # Ganti dengan IP atau hostname perangkat
+        "username": "",
+        "password": "",
     },
     {
         "device_type": "cisco_xr",
-        "host": "R3.STA.PE-MOBILE.2",
-        "username": "930435",
-        "password": "12345Qwer"
+        "host": "",
+        "username": "",
+        "password": ""
     },
 ]
