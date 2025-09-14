922bd342a5733dcd780299df130cd1cf456720af
Adelwin Nugroho Liandy <100504259+AdelwinNugroho@users.noreply.github.com>
2025-09-14 13:13:53 +0700

    Update devices.py

--- devices.py
+++ devices.py
@@ -3,39 +3,32 @@
 
 DEVICES = [
     {
-        "device_type": "cisco_ios",
-        "host": "192.168.1.1",  # IP perangkat yang sebenarnya (diakses dari jumphost)
-        "username": "admin",
-        "password": "your_ssh_password",
-        "secret": "your_enable_password"
+        "device_type": "cisco_xr",
+        "host": "R3.STA.PE-MOBILE.1",
+        "username": "930435",
+        "password": "12345Qwer"
     },
     {
-        "device_type": "cisco_ios",
-        "host": "192.168.1.2",
-        "username": "admin",
-        "password": "your_ssh_password",
-        "secret": "your_enable_password"
+        "device_type": "cisco_xr",
+        "host": "R3.STA.PE-MOBILE.2",
+        "username": "930435",
+        "password": "12345Qwer"
     },
     # Tambahkan perangkat Cisco lainnya di sini
 ]
 
 # Konfigurasi untuk Jumphost SSI
 JUMPHOST_1 = {
-    "device_type": "linux",
-    "host": "10.0.0.1",
-    "username": "jumphost1_user",
-    "password": "jumphost1_password"
+    "device_type": "windows",
+    "host": "100.75.49.116",
+    "username": "user1",
+    "password": "sisindokom@sshd1"
 }
 
 # Konfigurasi untuk Jumphost Telkom
 JUMPHOST_2 = {
-    "device_type": "linux",
-    "host": "10.0.0.2",
-    "username": "jumphost2_user",
-    "password": "jumphost2_password"
+    "device_type": "terminal",
+    "host": "10.62.170.56",
+    "username": "930435",
+    "password": "Razor301412"
 }
-
-# Perlu diingat:
-# - Pastikan Anda mengganti IP, username, password, dan secret
-# - Pastikan Jumphost SSI memiliki akses SSH ke Jumphost Telkom
-# - Pastikan Jumphost Telkom memiliki akses SSH ke semua perangkat di atas
